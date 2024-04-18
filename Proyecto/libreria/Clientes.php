5509464f7526b8adad3680b139b13ad2f1b4540c
LeoNardoz333 <leo.vel333@gmail.com>
2024-04-17 18:03:09 -0600

    Se implementó el crud para los Clientes

--- Proyecto/libreria/Clientes.php
+++ Proyecto/libreria/Clientes.php
@@ -9,18 +9,61 @@
         function Insertar(array $datos)
         {
             $con = new mysqli(s, u, p, bd);
+            $con->set_charset("utf8");
+            $q = $con->stmt_init();
+            $q->prepare("INSERT INTO clientes VALUES (null, ?, ?, ?)");
+            $q->bind_param('sss', $datos['nombre'], $datos['tipoAuto'], $datos['turno']);
+            $q->execute();
+            $q->close();
         }
         function Consultar($nombre)
         {
+            $id=0;
+            $nombret='';
+            $tipoAuto='';
+            $turno='';
+            $con = new mysqli(s,u,p,bd);
+            $con->set_charset("utf8");
+            $q = $con->stmt_init();
+            $q->prepare("SELECT * FROM clientes WHERE nombre LIKE ?");
+            $nombret='%'.$nombret.'%';
+            $q->bind_param('s', $fill);
+            $q->execute();
+            $q->bind_result($id, $nombret, $tipoAuto,$turno);
 
+            $rs = '<table class="table table-bordered table-striped"><thead><tr><th>ID</th><th>'.
+            'Nombre</th><th>Tipo de auto</th><th>Turno</th></tr></thead><tbody>';
+
+            while ($q->fetch()) {
+                $rs .= "<tr>
+                <td>$id</td>
+                <td>$nombret</td>
+                <td>$tipoAuto</td>
+                <td>$turno</td>
+                </tr>";
+            }
+
+            $q->close();
+            return $rs.'</tbody></table>';
         }
         function Modificar(array $datos)
         {
-
+            $con = new mysqli(s, u, p, bd);
+            $con->set_charset("utf8");
+            $q = $con->stmt_init();
+            $q->prepare("update clientes set nombre=?, tipoAuto=?, turno=? where id=?");
+            $q->bind_param('sss', $datos['nombre'], $datos['tipoAuto'], $datos['turno'], $datos['id']);
+            $q->execute();
+            $q->close();
         }
         function Borrar($id)
         {
-            
+            $con = new mysqli(s, u, p, bd);
+            $con->set_charset("utf8");
+            $q = $con->stmt_init();
+            $q->prepare("delete from clientes where id=$id");
+            $q->execute();
+            $q->close();
         }
     }
         /*function Insertar($maquina, $numSerie,$FechaCompra,$Costo,$tipo)
