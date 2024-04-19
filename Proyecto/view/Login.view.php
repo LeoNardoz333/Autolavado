aaa7fd926738ba5d7aef2f11882255965a6a9985
Carlos David Barrientos C <112111991+Carlos-Centeno14@users.noreply.github.com>
2024-04-18 19:50:21 -0600

    Estrucuta de html
    Se hicieron las paginas con la estructura de html

---
+++ Proyecto/view/Login.view.php
@@ -0,0 +1,21 @@
+<body class="fondo3">
+  <div class="wrapper fadeInDown d-flex align-items-center justify-content-center">
+    <div id="formContent" class="flex-column">
+      <div>
+        <p>Autolavado</p>
+      </div>
+      <div class="first mt-3 mb-3">
+        <img src="images/alavado.png" id="icon" alt="Logo" />
+      </div>
+      <form class="mt-3 mb-3">
+        <input type="text" id="login" class="fadeIn second" name="login" placeholder="Usuario">
+        <input type="text" id="password" class="fadeIn third" name="login" placeholder="Contrasena">
+        <button type="button" class="btn btn-primary mt-2 mb-2">Iniciar Sesion</button>
+        <button type="button" class="btn btn-info mt-2 mb-2">Registrarse</button>
+      </form>
+      <div id="formFooter">
+        <a class="underlineHover" href="#">Olvido su contrasena?</a>
+      </div>
+    </div>
+  </div>
+</body>
---
+++ Proyecto/view/Reportes.view.php
@@ -0,0 +1,21 @@
+<div class="header">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+						<div class="header-left">
+							<img src="images/alavado.png">
+						</div>
+						<div class="top-title">
+							<h6 class="logo-title animated pulse">Menu de reportes</h6>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="menu">
+							<li class="f-item-1"><a href="mh-1">Reporte Pagos</a></li>
+							<li class="f-item-2"><a href="mh-2">Reporte Servicios</a></li>
+							<li class="f-item-3"><a href="mh-3">Reporte Empleados</a></li>
+						</div>
+					</div>
+				</div>
+			</div>
+</div>
