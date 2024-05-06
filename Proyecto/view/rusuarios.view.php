7dc4a74158755e3d986373d244d7e54675f60bc5
Dell <yarelyperez628@gmail.com>
2024-05-06 17:34:32 -0600

    Diseño clientes usuarios

--- Proyecto/view/rusuarios.view.php
+++ Proyecto/view/rusuarios.view.php
@@ -2,20 +2,22 @@
   .fondo4
   {
     /* background-image: linear-gradient(90deg, #6dd5ed 10%, #4567e4 90%); */
-    background-image: linear-gradient(90deg, #000042 0%, #4567e4 89%);
+    background-image: linear-gradient(90deg, #93C3CE 0%, #D3E6EA 89%);
   }
 </style>
 <body class="fondo4">
-<div>Registro Usuarios</div>
+<div>
+    <img style="max-width: 70px; height: auto; display: inline-block; vertical-align: middle;" src="images/alavado.png">
+    <h2 style="color: navy; padding-left: 100px; display: inline-block; margin-left: 10px;">Registro Usuarios</h2>
+</div>
 <div class="header-left">
-<img src="images/alavado.png">
 </div>
 <div class="row">
     <div class="col-6" id="x">
         <form method="post" action="rusuarios">
-						
+						<br>
 						<div class="form-group">
-							<label for="name" class="cols-sm-2 control-label">Empleado</label>
+						<label for="name" class="cols-sm-2 control-label" style="font-size: larger; font-weight: bold;">Empleado</label>
 							<div class="cols-sm-10">
 								<div class="input-group">
 									<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
@@ -23,9 +25,8 @@
 								</div>
 							</div>
 						</div>
-
 						<div class="form-group">
-							<label for="username" class="cols-sm-2 control-label">Usuario</label>
+						<label for="username" class="cols-sm-2 control-label" style="font-size: larger; font-weight: bold;">Usuario</label>
 							<div class="cols-sm-10">
 								<div class="input-group">
 									<span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i></span>
@@ -33,9 +34,8 @@
 								</div>
 							</div>
 						</div>
-
 						<div class="form-group">
-							<label for="pass" class="cols-sm-2 control-label">Contraseña</label>
+						<label for="pass" class="cols-sm-2 control-label" style="font-size: larger; font-weight: bold;">Contraseña</label>
 							<div class="cols-sm-10">
 								<div class="input-group">
 									<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
@@ -54,14 +54,10 @@
 							</div>
 						</div> */
 						?>
-
 						<div class="form-group ">
 							<button type="submit" class="btn btn-primary btn-lg mt-3">Registrar</button>
+							<a href="login" class="btn btn-danger btn-lg mt-3">Cancelar</a>
 						</div>
-						<div class="form-group">
-							<a href="login" class="btn btn-danger btn-lg mt-3">Cancelar</a>         
-						</div>
-        </form>
     </div>
     <div class="col-6">
         <?php #echo $resultado; ?>
